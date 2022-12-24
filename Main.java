e294d5edb4a45fd82b0902c3f3acb40027fb8f29
Rickyslash <r.ckyslash@gmail.com>
2022-12-24 20:00:55 +0700

    add crud functionality

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String[] args) {
@@ -16,9 +18,45 @@ public class Main {
 
         System.out.println("\n********************************MOVIE STORE*******************************\n");
 
-        for (int i = 0; i < movies.length; i++) {
-            System.out.println(store.getMovie(i));
+        for (int i = 0; i < store.getStoreLength(); i++) {
+            System.out.println((i+1) + ".\t" + store.getMovie(i));
         }
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("\nTo edit a list, type 'continue': ");
+        String cont = scan.next();
+
+        while (cont.equals("continue")) {
+
+            System.out.print("\nPlease choose which index to change: ");
+            int index = scan.nextInt();
+            scan.nextLine();
+            index -= 1;
+
+            System.out.print("\nWhat's the name of the movie? ");
+            String movieName = scan.nextLine();
+
+            System.out.print("\nWhat's the format? ");
+            String movieFormat = scan.next();
+
+            System.out.print("\nAnd how's the rating? ");
+            double movieRating = scan.nextDouble();
+
+            store.setMovie(index, new Movie(movieName, movieFormat, movieRating));
+
+            System.out.print("\n********************************MOVIE STORE*******************************\n");
+
+            for (int i = 0; i < store.getStoreLength(); i++) {
+                System.out.println((i+1) + ".\t" + store.getMovie(i));
+            }
+
+            System.out.print("\nTo edit a list, type 'continue': ");
+            cont = scan.next();
+
+        }
+
+        scan.close();
 
     }
 
