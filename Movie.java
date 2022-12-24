2d833eed72977b45ae6b509742669aa0d6607a0e
Rickyslash <r.ckyslash@gmail.com>
2022-12-24 16:47:06 +0700

    make big 3 for Movie class

--- Movie.java
+++ Movie.java
@@ -1,5 +1,22 @@
 public class Movie {
-    
-    
+    private String name;
+    private String format;
+    private double rating;
 
+    public Movie (String name, String format, double rating) {
+        this.name = name;
+        this.format = format;
+        this.rating = rating;
+    }
+
+    public Movie (Movie source) {
+        source.name = name;
+        source.format = format;
+        source.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return this.rating + "\t" + this.format + "\t\t" + this.name;
+    }
 }
